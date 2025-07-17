73999233250acd39546134f1669ba9cc1c375de6
Henrique Santos <silvashrq@gmail.com>
2025-07-17 12:55:15 -0300

    up

--- aula 16/index.js
+++ aula 16/index.js
@@ -1,3 +1,16 @@
 //Indexados        0       1        2
-const alunos = ["Luiz", "Maria", "João"]; //Array
+const alunos = ["Luiz", "Maria", "João", "Junior"]; //Array
 //console.log(alunos);
+//console.log(alunos); //Maria
+
+//alunos.unshift("Luiza"); //Adcionar no começo
+//alunos.push("Otávio"); //Adicionar elemento NO FIM
+
+//alunos.pop(); //Remove o ultimo elemento
+//alunos.shift(); //Remove o do começo
+
+//delete alunos[1]; //Remove o elemento(mas não o índice)
+//Fatiar
+//console.log(alunos.slice(0, 2));
+
+console.log(alunos);
