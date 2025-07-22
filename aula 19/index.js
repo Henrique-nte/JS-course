8ee13d484ce2764f1dc4a0e52ce43f6266ac093b
Henrique Santos <silvashrq@gmail.com>
2025-07-22 12:21:17 -0300

    up

---
+++ aula 19/index.js
@@ -0,0 +1,7 @@
+/*
+Primitivos (imutáveis) - string, number, boolena, undefined, null (bigint, symbol) - Valores copiados
+
+
+Referencia (mutável) - Array, Object, function // Apontam pro mesmo endereço de memória
+ */
+  
