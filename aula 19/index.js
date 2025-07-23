565a09b85b8e224da8556d170c35c6d0712a01a2
Henrique Santos <silvashrq@gmail.com>
2025-07-23 15:10:34 -0300

    up

--- aula 19/index.js
+++ aula 19/index.js
@@ -4,4 +4,17 @@ Primitivos (imutáveis) - string, number, boolena, undefined, null (bigint, symb
 
 Referencia (mutável) - Array, Object, function // Apontam pro mesmo endereço de memória
  */
-  
+
+const a = {
+  nome: "Henrique",
+  sobrenome: "Santos",
+};
+//const b = a; Isso aqui aponta aponta pro mesmo local da memoria
+console.log(b);
+
+a.nome = "Joao";
+console.log(b);
+
+//Se eu quisesse copiar o valor de b para a
+//Spread
+const b = { ...a }; //agora o valor de b é independete
