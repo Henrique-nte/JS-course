e464c8193c5f4b3d67ce2d487b3247639f8388b8
Henrique Santos <silvashrq@gmail.com>
2025-07-24 15:05:49 -0300

    Up

--- aula 26/modelo/assets/js/main.js
+++ aula 26/modelo/assets/js/main.js
@@ -1,18 +1,38 @@
-const resultado = document.querySelector(".resultado");
-resultado.style.display = "none";
-
-function funcaoCalcular() {
+function funcaoPrincipal() {
   const form = document.querySelector(".form");
-  const botao = form.querySelector("#calcular");
+  const resultado = document.querySelector(".resultado");
+  resultado.style.display = "none";
 
-  //Pegando os valores
-  const peso = form.querySelector("#peso");
-  const altura = form.querySelector("#altura");
+  function calculaImc(evento) {
+    evento.preventDefault();
+    const peso = form.querySelector("#peso");
+    const altura = form.querySelector("#altura");
 
-  botao.addEventListener("click", function () {
-    let resul = peso.value / (altura.value * altura.value);
-    console.log(peso.value);
-  });
+    let valorPeso = peso.value;
+    let valorAltura = altura.value;
+    let resul = valorPeso / (valorAltura * valorAltura);
+
+    //Verificar se é valido
+    if (isNaN(Number(valorPeso))) {
+      resultado.innerHTML = "Peso inválido";
+      resultado.style.backgroundColor = "red";
+    } else if (isNaN(Number(valorAltura))) {
+      resultado.innerHTML = "Altura inválida";
+      resultado.style.backgroundColor = "red";
+    } else {
+      if (resul < 18.5) {
+        classificacao = "Abaixo do peso";
+      } else if (resul < 24.9) {
+        classificacao = "Peso Normal";
+      } else if (resul < 29.9) {
+        classificacao = "Sobrepeso";
+      }
+      resultado.style.backgroundColor = "";
+      resultado.innerHTML = `Seu IMC é ${resul.toFixed(2)} (${classificacao})`;
+    }
+
+    resultado.style.display = "block";
+  }
+  form.addEventListener("submit", calculaImc);
 }
-
-funcaoCalcular();
+funcaoPrincipal();
